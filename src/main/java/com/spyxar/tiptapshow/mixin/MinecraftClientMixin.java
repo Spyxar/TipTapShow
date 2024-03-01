1309f5879e0e45d6d612890e90fc104899ad14f1
Spyxar <81620237+Spyxar@users.noreply.github.com>
2024-03-01 21:48:03 +0100

    Revert a change to MinecraftClientMixin that limited click speed

--- src/main/java/com/spyxar/tiptapshow/mixin/MinecraftClientMixin.java
+++ src/main/java/com/spyxar/tiptapshow/mixin/MinecraftClientMixin.java
@@ -2,10 +2,7 @@ package com.spyxar.tiptapshow.mixin;
 
 import com.spyxar.tiptapshow.ClickCounter;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.GameOptions;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,22 +11,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin
 {
-    @Shadow
-    @Final
-    public GameOptions options;
-
     @Inject(at = @At(value = "HEAD"), method = "doAttack")
     private void onAttack(CallbackInfoReturnable<Boolean> cir)
     {
         ClickCounter.registerLeftClick();
     }
 
-    @Inject(at = @At(value = "HEAD"), method = "handleInputEvents")
-    private void onInputEvent(CallbackInfo ci)
+    @Inject(at = @At(value = "HEAD"), method = "doItemUse")
+    private void onItemUse(CallbackInfo ci)
     {
-        if (this.options.useKey.wasPressed())
-        {
-            ClickCounter.registerRightClick();
-        }
+        ClickCounter.registerRightClick();
     }
 }
