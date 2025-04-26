260ea00869550e88356510ca79a85dc1175d4158
Spyxar <spyxar@outlook.com>
2025-04-26 14:51:39 +0200

    Fix issue where holding RMB would cause cps to stay 5

---
+++ src/main/java/com/spyxar/tiptapshow/mixin/MouseMixin.java
@@ -0,0 +1,36 @@
+package com.spyxar.tiptapshow.mixin;
+
+import com.spyxar.tiptapshow.ClickCounter;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Mouse;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Mouse.class)
+public class MouseMixin
+{
+    @Inject(method = "onMouseButton", at = @At("HEAD"))
+    public void test(long window, int button, int action, int mods, CallbackInfo ci)
+    {
+        if (action != GLFW.GLFW_PRESS)
+        {
+            return;
+        }
+        if (MinecraftClient.getInstance().currentScreen != null)
+        {
+            return;
+        }
+
+        if (button == GLFW.GLFW_MOUSE_BUTTON_RIGHT)
+        {
+            ClickCounter.registerRightClick();
+        }
+        if (button == GLFW.GLFW_MOUSE_BUTTON_LEFT)
+        {
+            ClickCounter.registerLeftClick();
+        }
+    }
+}
